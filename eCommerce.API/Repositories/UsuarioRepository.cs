f322be168fb122b7c9c247d4d85a431ce81f0a77
Natheus <natheusmunes@gmail.com>
2022-12-12 21:41:56 -0300

    feat: add Migrations

--- eCommerce.API/Repositories/UsuarioRepository.cs
+++ eCommerce.API/Repositories/UsuarioRepository.cs
@@ -1,35 +1,41 @@
-﻿using eCommerce.Models;
+﻿using eCommerce.API.Database;
 
 namespace eCommerce.API.Repositories
 {
     public class UsuarioRepository : IUsuarioRepository
     {
-        public static List<Usuario> _db = new();
+        private readonly eCommerceContext _db;
+        public UsuarioRepository(eCommerceContext db)
+        {
+            _db = db;
+        }
 
         public List<Usuario> Get()
         {
-            return _db;
+            return _db.Usuarios.OrderBy(u => u.Id).ToList();
         }
 
         public Usuario Get(int id)
-        { 
-            return _db.Find(x => x.Id == id)!;
+        {
+            return _db.Usuarios.Find(id)!;
         }
 
         public void Add(Usuario usuario)
         {
             _db.Add(usuario);
+            _db.SaveChanges();
         }
 
         public void Update(Usuario usuario)
         {
-            _db.Remove(Get(usuario.Id));
-            _db.Add(usuario);
+            _db.Update(usuario);
+            _db.SaveChanges();
         }
 
         public void Delete(int id)
         {
             _db.Remove(Get(id));
+            _db.SaveChanges();
         }
     }
 }
